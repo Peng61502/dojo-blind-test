834a822b0d173ae080653510c7fa32059ca8c26c
Romain Malterre-Ngo <romain.malterre-ngo@student-cs.fr>
2025-02-14 19:31:08 +0100

    Manual transformation complete

--- scripts/generate-spotify-client.js
+++ scripts/generate-spotify-client.js
@@ -1,8 +1,10 @@
 import { mkdir, writeFile } from "fs/promises";
 
 import openapi from "../openapi.json" assert { type: 'json' };
+import { types } from "util";
 
 const targetDirectory = "src/lib/spotify/model";
+var imports = ""; // global varialbe for imported types
 
 async function generateSpotifyClient() {
   console.log("\nLaunched generate-spotify-client script");
@@ -20,32 +22,102 @@ async function generateSpotifyClient() {
 
 function generateType(typeName, typeSchema) {  
   console.log(`Generating type ${typeName}...`);
-
   const generatedCode = getGeneratedCode(typeName, typeSchema);
-
   writeFile(`${targetDirectory}/${typeName}.ts`, generatedCode);
+  imports = "";
 }
 
 function getGeneratedCode(typeName, typeSchema) {
   const generatedType = getGeneratedType(typeSchema);
-
-  return `export type ${typeName} = ${generatedType};`;
+  if (imports !== "") {
+    imports += "\n";
+  }
+  return `${imports}export type ${typeName} = ${generatedType};`;
 }
 
-function getGeneratedType(typeSchema) {
-  const schemaType = typeSchema.type;
+function getGeneratedType(typeSchema, ignoreType=false) { // ignoreType used for handling object types
+  var keyType = "";
 
-  // TO DO: Generate typescript code from schema
-  switch (schemaType) {
-    case "number":
-    case "integer":
-    case "string":
-    case "boolean":
-    case "array":
-    case "object":
-    default:
-      return "";
+  // Types réguliers
+  if (typeSchema.type !== undefined && !ignoreType) {
+    const schemaType = typeSchema.type;
+    switch (schemaType) {
+      case "number": return "number";
+      case "integer": return "number";
+      case "string": {
+        if (typeSchema.enum == undefined) {
+          return "string";
+        }
+        else {
+          for (const type of typeSchema.enum) {
+            keyType += "\"" + type + "\" | ";
+          }
+          return keyType.substring(0, keyType.length - 3);
+        }
+      }
+      case "boolean": return "boolean";
+      case "object": {
+        console.log("OBJECT")
+        // Get required
+        return getGeneratedType(typeSchema, true);
+      }
+      case "array": {
+        var items = typeSchema.items;
+        return getGeneratedType(items) + "[]"
+      }
+      default: {
+        return "";
+      }
+    }
+  }
+
+  // Properties
+  else if (typeSchema.properties !== undefined) {
+    const properties = typeSchema.properties;
+    var required = [];
+    var body = "{\n";
+    if (typeSchema.required !== undefined) {
+      required = typeSchema.required;
+    }
+    for (const key of Object.keys(properties)) {
+      keyType = getGeneratedType(properties[key]);
+      body += " " + key;
+      if (!required.includes(key)) {
+        body += "?"
+      }
+      body += ": " + keyType + ";\n";
+    }
+    return body + "}";
+  }
+
+  // Imported type
+  else if (typeSchema.$ref !== undefined) {6
+    keyType = typeSchema.$ref;
+    keyType = keyType.match(/[^/]+$/)[0];
+    if (!(imports.includes("import { " + keyType + " } from \"./" + keyType + "\";"))) 
+      imports += "import { " + keyType + " } from \"./" + keyType + "\";\n";
+    return keyType;
+  }
+
+  // oneOf
+  else if (typeSchema.oneOf !== undefined) {
+    keyType = "("
+    for (const type of typeSchema.oneOf) {
+      keyType += getGeneratedType(type) + " | ";
+    }
+    return keyType.substring(0, keyType.length - 3) + ")";
+  }
+
+  // allOf
+  else if (typeSchema.allOf !== undefined) {
+    console.log("ALLOF");
+    keyType = "";
+    for (const type of typeSchema.allOf) {
+      var genType = getGeneratedType(type);
+      console.log(genType);
+      keyType += genType +" & ";
+    }
+    return keyType.substring(0, keyType.length - 3);
   }
 }
-
 generateSpotifyClient();
